910dd648bde43eddaaa1b37ecca4339254605f0f
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-11-25 10:38:59 +0100

    Changed script to generate files using namespaces and uses

--- console/createClassesIncludes.php
+++
@@ -1,9 +0,0 @@
-<?php
-//add the inclusions
-$file = 'dynamicIncludes'.'.php';
-$current = "<?php\n";
-foreach ($objects as $object) {
-    $current .= "include_once 'app/Model/".ucfirst($object['objectName'])."Model.php';\n";   
-}
-file_put_contents($file, $current);
-?>
--- console/dynamicIncludes.php
+++
@@ -1,2 +0,0 @@
-<?php
-include_once 'app/Model/GamesModel.php';
