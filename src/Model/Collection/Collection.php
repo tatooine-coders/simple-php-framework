817ede223cdd7b429d83162ff7c3c8ed130629b7
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-12-05 15:03:49 +0100

    Updated Collection and Entity Classes with default methods

--- src/Model/Collection/Collection.php
+++ src/Model/Collection/Collection.php
@@ -1,9 +1,10 @@
 <?php
-
 namespace TC\Model\Collection;
 
-use TC\Lib\Database;
-
+use TC\Lib\DB;
+use PDO;
+use IteratorAggregate;
+use ArrayIterator;
 
 /**
  * This file is part of the Simple PHP Framework
@@ -16,18 +17,39 @@ use TC\Lib\Database;
  * @license  http://www.opensource.org/licenses/mit-license.php MIT License
  * @link     https://github.com/tatooine-coders/simple-php-framework/
  */
-class Collection
+class Collection implements IteratorAggregate
 {
+
+    protected $_table = null;
+    protected $_data = [];
 
     public function fetchAll()
     {
+        $query = 'SELECT * FROM ' . $this->_table;
+        $statement = DB::c()->prepare($query);
+        $statement->execute();
+
+        $statement->setFetchMode(PDO::FETCH_OBJ);
+        // Entity name
+        $entity = 'App\\Model\\Entity\\' . ucfirst($this->_table) . 'Entity';
+
+        while (!empty($row = $statement->fetch())) {
+            $this->_data[] = new $entity($row);
+        };
+    }
+
+    public function getIterator()
+    {
+        return new ArrayIterator($this->_data);
     }
 
     public function saveAll()
     {
+        
     }
 
     public function deleteAll()
     {
+        
     }
 }
--- src/Model/Entity/Entity.php
+++ src/Model/Entity/Entity.php
@@ -1,8 +1,8 @@
 <?php
 namespace TC\Model\Entity;
 
-use TC\Lib\Database;
-
+use TC\Lib\DB;
+use PDO;
 
 /**
  * This file is part of the Simple PHP Framework
@@ -23,39 +23,27 @@ class Entity
      *
      * @var mixed
      */
-    protected $data = [];
+    protected $_data = [];
+    protected $_fields = [];
 
     /**
      * Primary key name
      * @var string
      */
-    protected $primaryKey = 'id';
+    protected $_primaryKey = 'id';
 
     /**
-     * Default constructor
-     *
-     * @param array $data Arrays of keys=>data
+     * Table name
+     * @var string
      */
-    public function __construct($data = [])
-    {
-        $this->set($data);
-    }
+    protected $_tableName = null;
 
     /**
-     * Populates the array of data
-     *
-     * @param type $data
-     *
-     * @return void
+     * State of the current entity
+     * 
+     * @var boolean 
      */
-    public function set($data = [])
-    {
-        if (!empty($data)) {
-            foreach ($this->data as $field => $value) {
-                $this->__set($field, $value);
-            }
-        }
-    }
+    protected $_isModified = false;
 
     /**
      * Fetches an entity in DB and populates $fields
@@ -64,6 +52,19 @@ class Entity
      */
     public function fetch($id)
     {
+        $query = "SELECT * FROM `" . $this->_tableName . "` WHERE " . $this->_primaryKey . " = :id";
+        $statement = DB::c()->prepare($query);
+        $statement->bindParam('id', $id);
+
+        $statement->execute();
+        $statement->setFetchMode(PDO::FETCH_OBJ);
+        $row = $statement->fetch();
+        if (!empty($row)) {
+            $this->set($row, true);
+            $this->_isModified = false;
+        } else {
+            die('Invalid id');
+        }
     }
 
     /**
@@ -73,9 +74,87 @@ class Entity
      */
     public function save()
     {
-        // Check for id
+        $fieldList = $this->_fields;
+        unset($fieldList[array_search($this->_primaryKey, $fieldList)]);
 
-        // Save or update
+        if (empty($this->__get($this->_primaryKey))) {
+            // Fields list
+
+            $fields = join(',', $fieldList);
+
+            $attributes = join(',', array_map(function($e) {
+                    return ':' . $e;
+                }, $fieldList));
+            $query = "INSERT INTO " . $this->_tableName . ""
+                . " (" . $fields . ") VALUES (" . $attributes . ")";
+            $statement = DB::c()->prepare($query);
+
+            foreach ($fieldList as $field) {
+                $statement->bindParam($field, $this->_data[$field]);
+            }
+
+            $statement->execute();
+        } elseif ($this->_isModified) {
+            $list = join(', ', array_map(function($e) {
+                    return  $e . '= :' . $e;
+                }, $fieldList));
+            $query = "UPDATE " . $this->_tableName . " SET " . $list . " WHERE " . $this->_primaryKey . ' = :' . $this->_primaryKey;
+            $statement = DB::c()->prepare($query);
+            foreach ($fieldList as $field) {
+                $statement->bindParam($field, $this->_data[$field]);
+            }
+            $statement->bindParam($this->_primaryKey, $this->_data[$this->_primaryKey]);
+            $statement->execute();
+        }
+    }
+
+    public function getData()
+    {
+        return $this->_data;
+    }
+
+    /**
+     * Default constructor
+     *
+     * @param array $data Arrays of keys=>data
+     */
+    public function __construct($data = [])
+    {
+        $this->set($data, true);
+    }
+
+    /**
+     * Populates the array of data
+     *
+     * @param type $data
+     *
+     * @return void
+     */
+    public function set($data = [], $initial = false)
+    {
+        if (!empty($data)) {
+            foreach ($data as $field => $value) {
+                if ($field != $this->_primaryKey || $initial) {
+                    $this->__set($field, $value);
+                }
+            }
+        }
+    }
+
+    /**
+     * Generic setter
+     *
+     * @param string $field
+     * @param mixed  $value
+     */
+    public function __set($field, $value)
+    {
+        if (array_search($field, $this->_fields) !== false) {
+            if ($this->__get($field) != $value) {
+                $this->_isModified = true;
+            }
+            $this->_data[$field] = $value;
+        }
     }
 
     /**
@@ -87,19 +166,10 @@ class Entity
      */
     public function __get($field)
     {
-        return $this->data[$field];
-    }
-
-    /**
-     * Generic setter
-     *
-     * @param string $field
-     * @param mixed  $value
-     */
-    public function __set($field, $value)
-    {
-        if (key_exists($this->data, $field)) {
-            $this->data[$field] = $value;
+        if (key_exists($field, $this->_data)) {
+            return $this->_data[$field];
+        } else {
+            return null;
         }
     }
 }
