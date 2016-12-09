287e04d4903328433f4452d991669541bdd2c88c
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-12-09 11:12:19 +0100

    Updated Controller for View class

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -1,6 +1,8 @@
 <?php
 namespace TC\Controller;
 
+use TC\View\View;
+
 /**
  * This file is part of the Simple PHP Framework
  *
@@ -16,12 +18,42 @@ abstract class Controller
 {
 
     /**
+     * View object
+     * @var View
+     */
+    public $View = null;
+
+    /**
+     * Variables to be passed to the view
+     * @var array
+     */
+    public $viewVars = [];
+
+    /**
+     * Template to display
+     * @var string
+     */
+    public $template = null;
+
+    /**
+     * Constructor
+     * Instanciates the view
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->View = new View();
+    }
+
+    /**
      * Action that should be executed before the action
      *
      * @return void
      */
     public function beforeAction()
     {
+       // Generic logic here 
     }
 
     /**
@@ -31,5 +63,29 @@ abstract class Controller
      */
     public function afterAction()
     {
+        // Generic logic here
+    }
+
+    /**
+     * Sets a variable available in view.
+     * 
+     * @param string $variable Variable name
+     * @param mixed $value Variable content
+     * 
+     * @return void
+     */
+    public function set($variable, $value)
+    {
+        $this->viewVars[$variable] = $value;
+    }
+
+    /**
+     * Calls the renderer and pass it the params
+     * 
+     * @return void
+     */
+    public function render()
+    {
+        $this->View->render($this->template, $this->viewVars);
     }
 }
