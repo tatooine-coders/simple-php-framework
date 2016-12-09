03ccc6f84b77fa1c420759d044546704fb013c93
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-12-09 11:12:05 +0100

    Added a render() method

--- src/View/View.php
+++ src/View/View.php
@@ -15,4 +15,24 @@ namespace TC\View;
 class View
 {
 
+    /**
+     * Loads the view
+     * 
+     * @param string $template Path to /app/View/<path>.php
+     * @param array $variables Array of variable=>value
+     * 
+     * @return void
+     */
+    public function render($template, $variables)
+    {
+        extract($variables);
+        // Create path
+        $filename = './app/View/' . $template . '.php';
+        if (file_exists($filename)) {
+            // Make the variables available
+            include $filename;
+        } else {
+            die('View ' . $filename . ' not found');
+        }
+    }
 }
